001a004b630642e86fdf0ffce20b5621d95a17a6
Brian Abidin <Brian.abidin1@gmail.com>
2022-04-18 15:04:05 -0400

    change op functions and add prevOp variable

--- calc.js
+++ calc.js
@@ -4,6 +4,7 @@ let disValue = "";
 let res = 0;
 let equalDone = 1;
 let op;
+let prevOp;
 
 const add = function (){
     let sum = arguments[0];
@@ -51,21 +52,12 @@ const numPress = function(numID){
     
 }
 
-const opPress = function(opID){
-    if(`${opID}` === '+') opAdd();
-    if (`${opID}` === '-') opSub();
-    if (`${opID}` === '*') opMul();
-    if (`${opID}` === '/') opDiv();
-    if (`${opID}` === '=') opEqual();
-    if (`${opID}` === 'clear') reset();
-}
-
-const opAdd = function(){
+/* const opAdd = function(){
     equalDone = 0; // equals hasn't been clicked
     op = '+'; // currently in + operation
     if(res === 0){ //used for the first operation
         value = parseInt(disValue);
-        value2 = 0; 
+        value2 = 0;
     }
     res = add(value, value2);
     document.getElementById("display").value = res;
@@ -77,11 +69,12 @@ const opAdd = function(){
         document.getElementById("display").value = 0;
         reset();
     }
-}
+} 
 
 const opSub = function(){
     equalDone = 0;
     op = '-';
+    
     console.log(value);
     if(res === 0){
         value = parseInt(disValue);
@@ -97,7 +90,7 @@ const opSub = function(){
         document.getElementById("display").value = 0;
         reset();
     }
-}
+} */
 
 const opMul = function(){
     equalDone = 0;
@@ -109,7 +102,7 @@ const opMul = function(){
     res = multiply(value, value2);
     document.getElementById("display").value = res;
     value = res;
-    value2 = value;
+    value2 = 1;
     console.log(res, value, value2); 
     disValue = "";
     if(isNaN(document.getElementById("display").value)){
@@ -142,18 +135,30 @@ const opEqual = function(){
         res = add(value, parseInt(document.getElementById("display").value));
         document.getElementById("display").value = res;
         equalDone = 1;
+        value = res; 
+        disValue = "";
+        prevOp = "=";
     } else if(equalDone === 0 && op === '-'){
         res = subtract(value, parseInt(document.getElementById("display").value));
         document.getElementById("display").value = res;
         equalDone = 1;
+        value = res;
+        disValue = "";
+        prevOp = "=";
     } else if(equalDone === 0 && op === '*'){
         res = multiply(value, parseInt(document.getElementById("display").value));
         document.getElementById("display").value = res;
         equalDone = 1;
+        value = res;
+        disValue = "";
+        prevOp = "=";
     } else if(equalDone === 0 && op === '/'){
         res = divide(value, parseInt(document.getElementById("display").value));
         document.getElementById("display").value = res;
         equalDone = 1;
+        value = res; 
+        disValue = "";
+        prevOp = "=";
     }
 }
 
@@ -163,4 +168,73 @@ const reset = function(){
     value2 = 0;
     document.getElementById("display").value = 0;
     equalDone = 0;
+}
+
+const opPress = function(opID){
+    if(`${opID}` === '+') opAdd();
+    if (`${opID}` === '-') opSub();
+    if (`${opID}` === '*') opMul();
+    if (`${opID}` === '/') opDiv();
+    if (`${opID}` === '=') opEqual();
+    if (`${opID}` === 'clear') reset();
+}
+
+const opAdd = function(){
+    equalDone = 0; // equals hasn't been clicked
+    op = '+'; // currently in + operation
+    if(res === 0){ //used for the first operation
+        value = parseInt(disValue);
+        value2 = 0;
+        res = add(value, value2);
+        document.getElementById("display").value = res;
+        value = res;
+        value2 = value;
+        console.log(res, value, value2);
+        disValue = "";
+        prevOp = '+';
+    } else if(prevOp === '='){
+        prevOp = '+';
+    } else {
+        res = add(value, value2);
+        document.getElementById("display").value = res;
+        value = res;
+        value2 = value;
+        console.log(res, value, value2);
+        disValue = "";
+        prevOp = '+';
+    }
+    if(isNaN(document.getElementById("display").value)){
+        document.getElementById("display").value = 0;
+        reset();
+    }
+}
+
+const opSub = function(op){
+    equalDone = 0;
+    op = '-';
+    if(res === 0){
+        value = parseInt(disValue);
+        value2 = 0;
+        res = subtract(value, value2);
+        document.getElementById("display").value = res;
+        value = res;
+        value2 = value;
+        console.log(res, value, value2);
+        disValue = "";
+        prevOp = "-";
+    } else if(prevOp === '='){
+        prevOp = '-';   
+    } else {
+        res = subtract(value, value2);
+        document.getElementById("display").value = res;
+        value = res;
+        value2 = value;
+        console.log(res, value, value2);
+        disValue = "";
+        prevOp = "-";
+    }
+    if(isNaN(document.getElementById("display").value)){
+        document.getElementById("display").value = 0;
+        reset();
+    }
 }
